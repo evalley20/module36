972bd1dcb72e13ca538e4cc4bba7f06d00278bff
evalley20 <45612629+evalley20@users.noreply.github.com>
2019-01-04 11:51:49 -0600

    Update automatic_braking.py

--- automatic_braking.py
+++ automatic_braking.py
@@ -2,8 +2,8 @@ import RoboPiLib as RPL
 import setup
 def break()
   x = RPL.analogRead(4)
-  speed0 = -0.000776 * x ** 2 + 0.583894 * x + 1492.43
-  speed1 = -0.000776 * x ** 2 + 0.583894 * x + 1492.43
+  speed0 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
+  speed1 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
   print('this is the speed0', speed0)
   print('this is the speed1', speed1)
 
