d355b15da4b5b9f9cc1662a1d0b14140776187a8
evalley20 <45612629+evalley20@users.noreply.github.com>
2019-01-07 11:47:16 -0600

    Update automatic_braking.py

--- automatic_braking.py
+++ automatic_braking.py
@@ -1,6 +1,6 @@
 import RoboPiLib as RPL
 import setup
-def break()
+def brake()
   x = RPL.analogRead(4)
   speed0 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
   speed1 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
@@ -10,4 +10,4 @@ def break()
   if speed0 and speed1 >= 0:
     RPL.servoWrite(0,int(speed0)) 
     RPL.servoWrite(1,int(speed1))
-break()
+brake()
