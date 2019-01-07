142dc578825bf1d5703bc0845dc0d72e9f98b6c0
evalley20 <45612629+evalley20@users.noreply.github.com>
2019-01-07 11:42:14 -0600

    Update wall_touching.py

--- wall_touching.py
+++ wall_touching.py
@@ -2,8 +2,8 @@ import RoboPiLib as RPL
 import setup
 def break()
   x = RPL.analogRead(4)
-  speed0 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
-  speed1 = - 0.000885 * x ** 2 + 0.558286 * x + 1481.5
+  speed0 = 0.000237 * x ** 2 - 0.0172 * x + 1499.81
+  speed1 = 0.000237 * x ** 2 - 0.0172 * x + 1499.81
   print('this is the speed0', speed0)
   print('this is the speed1', speed1)
 
